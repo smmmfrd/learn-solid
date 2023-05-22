c588cd9919b2404bf1286f1de22837922fa8415c
smmmfrd <smmmfrd@gmail.com>
2023-05-22 11:19:13 -0700

    Locking Die and Style Change

--- src/tenzies/Tenzies.jsx
+++ src/tenzies/Tenzies.jsx
@@ -18,16 +18,29 @@ export default function Tenzies() {
 		setDice(prevDice => prevDice.map(die => ({
 			...die,
 			value: die.locked ? die.value : rollDie(),
-		})))
+		})));
 	}
 
 	return (
 		<>
 			<h1>Tenzies</h1>
-			<For each={dice()} fallback={<p>no values</p>}>{(die, index) =>
-				<button>
-					{die.value}
-				</button>
+			<For each={dice()} fallback={<p>no values</p>}>{(die, index) => {
+				const locked = die.locked;
+				return (
+					<button
+						style={{
+							"color": `${locked ? "red" : "black"}`
+						}}
+						onClick={() => {
+							setDice(prevDice => prevDice.map((die, i) => ({
+								...die,
+								locked: i === index() ? !locked : die.locked
+							})))
+						}}>
+						{die.value}
+					</button>
+				)
+			}
 			}</For>
 			<button onClick={handleRoll}>Roll Dice</button>
 		</>
